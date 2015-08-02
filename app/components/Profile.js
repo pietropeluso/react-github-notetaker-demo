c48fa5d962665fe486b18856ea5576a3ece08b3f
Pietro Peluso <pietro.peluso@gmail.com>
2015-08-02 17:28:48 +0100

    add the AddNote component and adding new note feature

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -23,6 +23,9 @@ var Profile = React.createClass({
       repos : []
     }
   },
+  handleNewNote: function(newNote) {
+    this.ref.child(this.getParams().username).set(this.state.notes.concat([newNote]));
+  },
   render: function() {
     var username = this.getParams().username;
     return (
@@ -34,7 +37,7 @@ var Profile = React.createClass({
           <Repos username={username} repos={this.state.repos} />
         </div>
         <div className="col-md-4">
-          <Notes username={username} notes={this.state.notes} />
+          <Notes username={username} notes={this.state.notes} addNote={this.handleNewNote} />
         </div>
       </div>
     )
