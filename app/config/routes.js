053c154ad7c822bb43f0761af659d72cb79c2375
Pietro Peluso <pietro.peluso@gmail.com>
2015-08-14 19:39:45 +0200

    refactoring App, config and utils files to ES6

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,13 +1,11 @@
-var React = require('react');
-var Router = require('react-router');
-var DefaultRoute = Router.DefaultRoute;
-var Route = Router.Route;
-var Main = require('../components/Main');
-var Home = require('../components/Home');
-var Profile = require('../components/Profile');
+import React from 'react';
+import Main from '../components/Main';
+import Home from '../components/Home';
+import Profile from '../components/Profile';
+import {Router, DefaultRoute, Route} from 'react-router';
 
 
-module.exports = (
+export default (
   <Route name="app" path="/" handler={Main}>
     <Route name="profile" path="profile/:username" handler={Profile} />
     <DefaultRoute handler={Home} />
--- app/utils/helper.js
+++ app/utils/helper.js
@@ -1,23 +1,23 @@
-var axios = require('axios');
+import axios from 'axios';
 
 function getRepos(username) {
-  return axios.get("https://api.github.com/users/" + username + "/repos");
+  return axios.get(`https://api.github.com/users/${username}/repos`);
 }
 
 function getUserInfo(username) {
-  return axios.get("https://api.github.com/users/" + username );
+  return axios.get(`https://api.github.com/users/${username}`);
 }
 
 var helpers = {
-  getGithubData: function(username) {
+  getGithubData(username) {
     return axios.all([getRepos(username), getUserInfo(username)])
-            .then(function(arr) {
-              return {
-                repos: arr[0].data,
-                bio: arr[1].data
-              }
-            });
+      .then( (arr) => {
+        return {
+          repos: arr[0].data,
+          bio: arr[1].data
+        }
+      });
   }
 };
 
-module.exports = helpers;
+export default helpers;
