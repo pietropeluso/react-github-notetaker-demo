a9a4646203cf510ffde1248535f0970a8d4ce7a9
Pietro Peluso <pietro.peluso@gmail.com>
2015-08-16 11:51:02 +0200

    fixing repositories URL rendered by the Repos component

--- app/components/Github/Repos.js
+++ app/components/Github/Repos.js
@@ -5,7 +5,7 @@ class Repos extends React.Component {
     var repos = this.props.repos.map((repo, index) => {
       return (
         <li className="list-group-item" key={index}>
-          {repo.html_url && <h4><a href="{repo.html_url}">{repo.name}</a></h4>}
+          {repo.html_url && <h4><a href={repo.html_url}>{repo.name}</a></h4>}
           {repo.description && <p>{repo.description}</p>}
         </li>
       );
